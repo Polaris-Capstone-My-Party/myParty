ac79c1843b0cb2036a5363705958e026308519c6
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-10-07 14:36:16 -0500

    Member model complete

---
+++ src/main/java/com/myParty/models/Member.java
@@ -0,0 +1,40 @@
+package com.myParty.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "members")
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter private Long id;
+
+    @Column(nullable = false)
+    @Getter @Setter private String username;
+
+    @Column(nullable = false)
+    @Getter @Setter private String email;
+
+    @Column(nullable = false)
+    @Getter @Setter private String password;
+
+    @Column(nullable = false)
+    @Getter @Setter private Long phone;
+
+    @Column(nullable = false)
+    @Getter @Setter private String first_name;
+
+    @Column(nullable = false)
+    @Getter @Setter private String last_name;
+
+
+}
