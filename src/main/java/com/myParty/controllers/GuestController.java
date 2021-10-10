e50f8e1aa83ba989fbb5aec6a5da0a8a2343a4ed
calaisgalbraith <calaisgalbraith@gmail.com>
2021-10-10 16:04:06 -0500

    come back to

--- src/main/java/com/myParty/controllers/GuestController.java
+++ src/main/java/com/myParty/controllers/GuestController.java
@@ -2,6 +2,7 @@ package com.myParty.controllers;
 
 import com.myParty.models.Guest;
 import com.myParty.repositories.GuestRepository;
+import com.myParty.repositories.PartyItemRepository;
 import com.myParty.repositories.PartyRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,31 +14,43 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class GuestController {
 
-    private final PartyRepository partyDAO;
-    private final GuestRepository guestDAO;
-
-    public GuestController(PartyRepository partyDAO, GuestRepository guestDAO) {
-        this.partyDAO = partyDAO;
-        this.guestDAO = guestDAO;
-    }
-
-    //show form for RSVPing
-    @GetMapping(path = "/rsvp/{partyID}") //TODO: add party identifying url link when logic done
-    public String showRSVPToGuest(@PathVariable long partyID, Model model) {
-        model.addAttribute("guest", new Guest()); //create new Guest object for form
-        model.addAttribute("party", partyDAO.getById(partyID)); //gets & sets party info
-        return "/guests/rsvp";
-    }
-
-    //create guest when submitting RSVP form
-    @PostMapping(path = "/rsvp")
-    public String saveGuestInfo(@ModelAttribute Guest guest, Model model) {
-            guestDAO.save(guest); //Create new guest instance
-            model.addAttribute("guest", guest);
-            //todo save party items via list?
-            //todo generate random url link mapped to guest
-        return "/guests/successRsvp";
-    }
+//    private final PartyRepository partyDAO;
+//    private final GuestRepository guestDAO;
+//    private final PartyItemRepository partyItemDAO;
+//
+//    public GuestController(PartyRepository partyDAO, GuestRepository guestDAO, PartyItemRepository partyItemDAO) {
+//        this.partyDAO = partyDAO;
+//        this.guestDAO = guestDAO;
+//        this.partyItemDAO = partyItemDAO;
+//    }
+//
+//    //show form for RSVPing
+//    @GetMapping(path = "/rsvp/{partyID}") //TODO: change to party identifying url link when logic done
+//    public String showRSVPToGuest(@PathVariable long partyID, Model model) {
+//        model.addAttribute("guest", new Guest()); //create new Guest object for form
+//        model.addAttribute("party", partyDAO.getById(partyID)); //gets & sets party info to view on form
+//
+//
+//
+//
+//        //TODO: get & set party items
+//        return "/guests/rsvp";
+//    }
+//
+//    //create guest when submitting RSVP form
+//    @PostMapping(path = "/rsvp/{partyID}")
+//    public String saveGuestInfo(@ModelAttribute Guest guest, Model model) {
+//            guestDAO.save(guest); //Create new guest instance
+//            model.addAttribute("guest", guest);
+//
+//
+//
+//
+//            //todo save party items via list?
+//            //todo update partyItems quantity required list
+//            //todo generate random url link mapped to guest
+//        return "/guests/successRsvp";
+//    }
 
 
 }
--- src/main/java/com/myParty/repositories/PartyItemRepository.java
+++ src/main/java/com/myParty/repositories/PartyItemRepository.java
@@ -4,4 +4,5 @@ import com.myParty.models.PartyItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PartyItemRepository extends JpaRepository<PartyItem, Long> {
+//    PartyItem findByPartyID(Long id);
 }
