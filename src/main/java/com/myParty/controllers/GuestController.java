d643369f236e203d3ae243dc28c247ec68fe73ae
calaisgalbraith <calaisgalbraith@gmail.com>
2021-10-13 10:50:03 -0500

    finished show RSVP page & allow guests to RSVP

--- src/main/java/com/myParty/controllers/GuestController.java
+++ src/main/java/com/myParty/controllers/GuestController.java
@@ -1,9 +1,11 @@
 package com.myParty.controllers;
 
 import com.myParty.models.Guest;
+import com.myParty.models.Party;
 import com.myParty.models.RsvpStatuses;
 import com.myParty.repositories.GuestRepository;
 import com.myParty.repositories.ItemBringerRepository;
+import com.myParty.repositories.PartyItemRepository;
 import com.myParty.repositories.PartyRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,13 +19,12 @@ public class GuestController {
 
     private final PartyRepository partyDAO;
     private final GuestRepository guestDAO;
-    private final ItemBringerRepository itemBringerDAO; //unsure if needed TBD
+    private final PartyItemRepository partyItemDAO;
 
-
-    public GuestController(PartyRepository partyDAO, GuestRepository guestDAO, ItemBringerRepository itemBringerDAO){
+    public GuestController(PartyRepository partyDAO, GuestRepository guestDAO, PartyItemRepository partyItemDAO){
         this.partyDAO = partyDAO;
         this.guestDAO = guestDAO;
-        this.itemBringerDAO = itemBringerDAO;
+        this.partyItemDAO = partyItemDAO;
     }
 
     //show RSVP form with corresponding Party Information
@@ -34,59 +35,53 @@ public class GuestController {
         rsvpStatuses.add("maybe");
         rsvpStatuses.add("no");
 
-        model.addAttribute("party", partyDAO.getByUrlKey(urlKey)); //gets party info for form
+        Party party = partyDAO.getByUrlKey(urlKey); // gets party info for form
+
+        model.addAttribute("party", party); //sets party info for form
         model.addAttribute("guest", new Guest()); //thing to allow form to recognize new guest
         model.addAttribute("rsvps", rsvpStatuses); //allows access to rsvp enum in form
-
-        //TODO: Show party items
+        model.addAttribute("partyItems", partyItemDAO.getByParty(party)); //gets & sets party Items for party
         return "guests/rsvp";
     }
 
-    //saves Guest information
+    //saves Guest information //check with herman about this
     @PostMapping(path = "/rsvp/{urlKey}")
     public String createGuest(@PathVariable String urlKey, @ModelAttribute Guest guest, @RequestParam String rsvp){
         guest.setRsvpStatus(RsvpStatuses.valueOf(rsvp)); //set RSVP status enum
         guest.setParty(partyDAO.getByUrlKey(urlKey)); //sets Party linked to guest
 
         UUID uuid = UUID.randomUUID(); // creates UUID unique for given guest
-        guest.setGuestKey(uuid.toString()); //https://www.baeldung.com/java-uuid
+        guest.setGuestKey(uuid.toString()); //https://www.baeldung.com/java-uui
 
         guestDAO.save(guest); //save guest information
 
-        //TODO: Create New instance of Item Bringer to link guest & Items they've signed up for?
-            //make nullable, be separate part
-        //TODO: Update Party Items after guests sign up for parties
+        return "redirect:/guests/successRsvp";
+    }
 
+    //shows RSVPSuccess page
+    @GetMapping(path = "/guests/successRsvp")
+    public String showRSVPSuccess(){
         return "guests/successRsvp";
     }
 
+    //TODO: Create New instance of Item Bringer to link guest & Items they've signed up for?  //make nullable, be separate part
+    //TODO: Update Party Items after guests sign up for parties
 
-
-
-
-
-
-
-
-
-
-
-
-    //Shows Guest Info & Allows to Edit
-    @GetMapping(path = "/rsvp/{urlKey}/{guestKey}")
-    public String showEditRSVP(@PathVariable String urlKey, @PathVariable String guestKey, Model model){
-        model.addAttribute("party", partyDAO.getByUrlKey(urlKey)); //get party info
-        model.addAttribute("guest", guestDAO.getByGuestKey(guestKey)); //get guest info
-        //TODO: Should a new instance of guest be allowed or just save it?
-        return "guests/editRsvp";
-    }
-
-    //saves Guest edited information
-    @PostMapping(path = "/rsvp/{url_key}/{guest_key}")
-    public String saveEditRSVP(){
-        //TODO: save updated guest info
-        //TODO: Update ItemBringer information (?)
-        //TODO: Update party items database
-        return "guests/successRsvp";
-    }
+//    //Shows Guest Info & Allows to Edit
+//    @GetMapping(path = "/rsvp/{urlKey}/{guestKey}")
+//    public String showEditRSVP(@PathVariable String urlKey, @PathVariable String guestKey, Model model){
+//        model.addAttribute("party", partyDAO.getByUrl_key(urlKey)); //get party info
+//        model.addAttribute("guest", guestDAO.getByGuestKey(guestKey)); //get guest info
+//        //TODO: Should a new instance of guest be allowed or just save it?
+//        return "guests/editRsvp";
+//    }
+//
+//    //saves Guest edited information
+//    @PostMapping(path = "/rsvp/{url_key}/{guest_key}")
+//    public String saveEditRSVP(){
+//        //TODO: save updated guest info
+//        //TODO: Update ItemBringer information (?)
+//        //TODO: Update party items database
+//        return "guests/successRsvp";
+//    }
 }
--- src/main/java/com/myParty/repositories/PartyItemRepository.java
+++ src/main/java/com/myParty/repositories/PartyItemRepository.java
@@ -1,7 +1,11 @@
 package com.myParty.repositories;
 
+import com.myParty.models.Party;
 import com.myParty.models.PartyItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PartyItemRepository extends JpaRepository<PartyItem, Long> {
+    List<PartyItem> getByParty(Party party);
 }
