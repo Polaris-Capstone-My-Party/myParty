8bfc12a34c8c21d6fb82b759dc2460c4eba68ad3
calaisgalbraith <calaisgalbraith@gmail.com>
2021-10-26 15:52:30 -0500

    finished logic show guests & partymembers to host

--- src/main/java/com/myParty/repositories/PartyMemberRepository.java
+++ src/main/java/com/myParty/repositories/PartyMemberRepository.java
@@ -1,7 +1,11 @@
 package com.myParty.repositories;
 
+import com.myParty.models.Party;
 import com.myParty.models.PartyMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PartyMemberRepository extends JpaRepository<PartyMember, Long> {
+    List<PartyMember> getByParty(Party party);
 }
