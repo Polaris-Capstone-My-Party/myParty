5924b1be94858a65eb14ca177fd8517a076052ac
Brittany Richards <brittany.m.richards3@gmail.com>
2021-11-02 11:08:24 -0500

    forgotpassword email works

--- src/main/java/com/myParty/controllers/ResetPasswordController.java
+++ src/main/java/com/myParty/controllers/ResetPasswordController.java
@@ -1,6 +1,9 @@
 package com.myParty.controllers;
 
 import com.myParty.models.Member;
+import com.myParty.repositories.MemberRepository;
+import com.myParty.services.EmailService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +12,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.mail.MessagingException;
 import java.util.UUID;
 
+@Controller
 public class ResetPasswordController {
+    private final MemberRepository memberDao;
+    private final EmailService emailService;
+
+    public ResetPasswordController(MemberRepository memberDao, EmailService emailService){
+        this.memberDao = memberDao;
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/member/forgotpassword")
+    public String forgotPW(){
+        return "member/forgotpassword";
+    }
 
     @GetMapping("/member/resetpassword/{token}")
     //TODO: make sure this works and create the form
@@ -18,17 +34,24 @@ public class ResetPasswordController {
         return "member/resetpassword";
     }
 
+    @PostMapping("/member/resetpassword/{token}")
+    public String resetPassword{
+
+    }
+
     @PostMapping("/member/resetpassword")
-    public String resetPassword(@RequestParam("email") String memberEmail) throws MessagingException {
+    public String resetPasswordEmail(@RequestParam(name = "email") String memberEmail) throws MessagingException {
         Member member = memberDao.getByEmail(memberEmail);
 
         String token = UUID.randomUUID().toString();
         member.setResetToken(token);
         memberDao.save(member);
-        String resetDetails = "";
+        String resetDetails = "Click to reset your password" + "<a href=\"http://localhost:8080/member/resetpassword/" + token + "\">HERE</a>";
 //TODO: add domain + the token in an anchor tag
         emailService.sendResetPassword(member, resetDetails);
 
-        return "";
+        return "redirect:/login";
     }
+
+
 }
